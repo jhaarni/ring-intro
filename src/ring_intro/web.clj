63daaf0f1d18da365286f178bd129aa6f4bbc783
Janne Haarni <janne.haarni@elisa.fi>
2013-10-07 15:13:08 +0300

    minor changes

--- src/ring_intro/web.clj
+++ src/ring_intro/web.clj
@@ -1,16 +1,19 @@
 (ns ring-intro.web
-  (:require   [noir.core :refer [defpage defpartial]]
+  (:require       
+    [noir.core :refer [defpage defpartial]]
     [hiccup.core :refer :all]
     [hiccup.page :refer :all]
+    [hiccup.form :refer :all]
     [hiccup.element :refer :all]))
 
 (defpartial layout [& content]
   (html
     [:head
-    [:title "Der Ring des Nibelungen"]]
-                                     ;^(include-css "some.css")]
-    [:body
-    content]))
+      [:title "Der Ring des Nibelungen"] 
+      (include-css "http://yui.yahooapis.com/pure/0.3.0/base-min.css")
+      (include-css "http://yui.yahooapis.com/pure/0.3.0/pure-min.css")]
+    [:body content]))
+
 
 (defpage [:get "/"] {}
-  (layout [:h1 "Hello Ring!"]))
+  (layout [:h1 "Hello Clojure!"]))
